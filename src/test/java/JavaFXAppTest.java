aa430af27b3f350ff60af5ccdfa798f57747e3f7
Misha Opstal <6018369@mborijnland.nl>
2024-11-13 12:07:16 +0100

    Implemented the addition

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,10 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+
+    @Test
+    void additionTest() {
+        JavaFXApp app = new JavaFXApp();
+        assertEquals(3, app.computeAdd(1, 2));
+    }
 }
