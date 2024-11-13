67592b3aea9d98b6ca68f59e54a8b0e29a5205e0
Misha Opstal <6018369@mborijnland.nl>
2024-11-13 12:09:22 +0100

    Update JavaFXAppTest.java

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -10,6 +10,23 @@ class JavaFXAppTest {
     @Test
     void additionTest() {
         JavaFXApp app = new JavaFXApp();
+
+        // Check positive numbers
         assertEquals(3, app.computeAdd(1, 2));
+        assertEquals(5, app.computeAdd(2, 3));
+        assertEquals(7, app.computeAdd(3, 4));
+
+        // Check negative numbers
+        assertEquals(-1, app.computeAdd(-1, 0));
+        assertEquals(-3, app.computeAdd(-1, -2));
+        assertEquals(-5, app.computeAdd(-2, -3));
+
+        // Check mixed numbers
+        assertEquals(0, app.computeAdd(-1, 1));
+        assertEquals(1, app.computeAdd(-1, 2));
+        assertEquals(2, app.computeAdd(1, 1));
+
+        // Check zero
+        assertEquals(1, app.computeAdd(1, 0));
     }
 }
